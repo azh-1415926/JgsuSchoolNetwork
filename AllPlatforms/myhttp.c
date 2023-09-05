1b738f636e4a4f1b01fa871e849c66309e786f16
azh-1415926 <AzhandHJ@outlook.com>
2023-09-05 23:57:44 +0800

    为 myhttp 添加注释，并调整缩进和部分代码

--- AllPlatforms/myhttp.c
+++ AllPlatforms/myhttp.c
@@ -1,107 +1,182 @@
 #include "myhttp.h"
+
+#if __linux__
+
+/* Linux 平台所用系统调用的头文件 */
+#include<fcntl.h>
+#include<unistd.h>
+
+#endif
+
 #define ACCOUNT "?user_account=%2C0%2C"
 #define PASSWORD "&user_password="
+
+/* acount 用于存储学号、passwd 存储密码，多一位字符用于存储 '\0' */
 static char account[11];
 static char passwd[21];
-void getUserInfo(char* buf){
+
+/* 获取用户账号、密码，填充到 buf 中 */
+void getUserInfo(char* buf)
+{
+    /* 清空 acount、passwd */
     memset(account,0,sizeof(account));
     memset(passwd,0,sizeof(passwd));
+    /* Windows 用库函数读取 passwd.txt 文件 */
     #if _WIN32
+        /* 只读方式打开文件 */
         FILE* f=fopen("passwd.txt","r");
-        if(f==NULL){
+        /* f 为空即 passwd.txt 文件不存在 */
+        if(f==NULL)
+        {
+            /* 没有便创建一个 passwd.txt */
             f=fopen("passwd.txt","w+");
+            /* 读取用户输入的 10 位的学号 */
             printf("Please enter your account[10]:");
             scanf("%10c",account);
-            fflush(stdin);
+            /* 读走缓冲区剩余字符 */
+            scanf("%*s");
+            /* 读取用户输入的密码 */
             printf("Please enter your passwd[*]:");
             scanf("%s",passwd);
-            fflush(stdin);
+            /* 将 acount、passwd 写入文件中 */
             fwrite(account,strlen(account),1,f);
             fwrite(passwd,strlen(passwd),1,f);
-        }else{
+        }
+        /* f 不为空即 passwd.txt 文件存在，读取文件中的学号、密码 */
+        else
+        {
             fread(account,10,1,f);
             fread(passwd,sizeof(passwd),1,f);
         }
+        /* 关闭文件 */
         fclose(f);
     #elif __linux__
+        /* fd 为读取或写入 passwd.txt 文件的描述符 */
         int fd;
-        if((fd=open("passwd.txt",O_RDONLY))>0){
+        /* fd 大于 0，即 passwd.txt 文件存在 */
+        if((fd=open("passwd.txt",O_RDONLY))>0)
+        {
+            /* 读取前十个字符作为学号 */
             int ret=read(fd,account,10);
-            if(ret<10){
+            /* 若字符不足 10 位，报错退出 */
+            if(ret<10)
+            {
                 close(fd);
                 perror("Account error!");
                 exit(-1);
             }
+            /* 剩余字符作为密码 */
             read(fd,passwd,sizeof(passwd));
-        }else{
+        }
+        /* fd 小于等于 0，即 passwd.txt 文件不存在 */
+        else
+        {
+            /* 以读写方式打开 passwd.txt 文件，没有便创建，权限为 0777 */
             fd=open("passwd.txt",O_RDWR|O_CREAT,777);
+            /* 读取用户输入的 10 位的学号 */
             printf("please input account(10):");
             scanf("%10c",account);
+            /* 读走缓冲区剩余字符 */
+            scanf("%*s");
+            /* 读取用户输入的密码 */
             printf("please input password(-)");
             scanf("%s",passwd);
+            /* 将 account 写入文件中，若不足 10 位，删除文件，报错退出 */
             int count=write(fd,account,10);
-            if(count<10){
+            if(count<10)
+            {
                 close(fd);
                 unlink("passwd.txt");
                 perror("Account error!");
                 exit(-1);
             }
+            /* 将 passwd 写入文件中 */
             write(fd,passwd,strlen(passwd));
         }
+        /* 关闭文件 */
         close(fd);
     #endif
+    /* 拼接学号参数名、学号，密码参数名、密码 */
     strcat(buf,ACCOUNT);
     strcat(buf,account);
     strcat(buf,PASSWORD);
     strcat(buf,passwd);
 }
-void httpMessageInitalize(struct httpMessage* pMessage,int size,const char* method,const char* url,const char* version,const char* host){
-    if(pMessage==NULL){
+
+/* 初始化报文，根据传入的请求方式、url、http 版本、host 初始化 */
+void httpMessageInitalize(struct httpMessage* pMessage,int size,const char* method,const char* url,const char* version,const char* host)
+{
+    /* 若报文指针为空，直接返回 */
+    if(pMessage==NULL)
+    {
         printf("httpMessage is null,intitalize error!\n");
         return;
     }
+    /* 清空报文 */
     memset(pMessage,0,size);
+    /* 插入报文头 */
     pMessage->method=(char*)method;
     pMessage->url=(char*)url;
     pMessage->version=(char*)version;
+    /* 插入 Host 字段 */
     pMessage->pField=(struct field_name*)malloc(sizeof(struct field_name));
     pMessage->pField->key="Host";
     pMessage->pField->value=(char*)host;
     pMessage->pField->next=NULL;
 }
-void httpMessageFree(struct httpMessage* pMessage){
-    if(pMessage==NULL){
+
+/* 释放报文 */
+void httpMessageFree(struct httpMessage* pMessage)
+{
+    /* 若报文指针为空，直接返回 */
+    if(pMessage==NULL)
+    {
         printf("httpMessage is null,free error!\n");
         return;
     }
+    /* pField 指向字段，pTemp 指向需要释放的字段 */
     struct field_name* pField=pMessage->pField;
     struct field_name* pTemp=NULL;
-    while(pField->next){
+    while(pField->next)
+    {
         pTemp=pField;
         pField=pField->next;
         free(pTemp);
     }
     free(pField);
 }
-void httpMessageAddField(struct httpMessage* pMessage,const char*key,const char*value){
-    if(pMessage==NULL){
+
+/* 为报文中添加字段，根据传入的 key、value 添加 */
+void httpMessageAddField(struct httpMessage* pMessage,const char*key,const char*value)
+{
+    /* 若报文指针为空，直接返回 */
+    if(pMessage==NULL)
+    {
         printf("httpMessage is null,addField error!\n");
         return;
     }
+    /* pField 指向字段 */
     struct field_name* pField=pMessage->pField;
-    while(pField->next){
+    /* 遍历到最后一个字段 */
+    while(pField->next)
         pField=pField->next;
-    }
+    /* 在最后一个字段插入新字段 */
     pField->next=(struct field_name*)malloc(sizeof(struct field_name));
     pMessage->pField->key=(char*)key;
     pMessage->pField->value=(char*)value;
     pMessage->pField->next=NULL;
 }
-void createHttpMessage(char *buf,struct httpMessage* pMessage){
-    if(pMessage==NULL){
+
+/* 创建报文 */
+void createHttpMessage(char *buf,struct httpMessage* pMessage)
+{
+    /* 若报文指针为空，直接返回 */
+    if(pMessage==NULL)
+    {
         printf("httpMessage is null,create error!\n");
         return;
     }
+    /* 插入报文头到 buf */
     strcat(buf, pMessage->method);
     strcat(buf, " ");
     strcat(buf, pMessage->url);
@@ -109,17 +184,19 @@ void createHttpMessage(char *buf,struct httpMessage* pMessage){
     strcat(buf, " ");
     strcat(buf, pMessage->version);
     strcat(buf, "\n");
+    /* 插入字段到 buf */
     struct field_name* pField=pMessage->pField;
-    do{
+    do
+    {
         strcat(buf, pField->key);
         strcat(buf,":");
         strcat(buf, pField->value);
         strcat(buf, "\n");
-        if(pField->next){
+        if(pField->next)
             pField=pField->next;
-        }else{
+        else
             pField=NULL;
-        }
-    }while(pField);
+    }
+    while(pField);
     strcat(buf, "\r\n");
 }
--- AllPlatforms/myhttp.h
+++ AllPlatforms/myhttp.h
@@ -1,27 +1,31 @@
 #ifndef _MY_HTTP_H_
 #define _MY_HTTP_H_
+
+/* 通用头文件 */
 #include<stdio.h>
 #include<string.h>
 #include<stdlib.h>
-#if _WIN32
-#elif __linux__
-#include<fcntl.h>
-#include<unistd.h>
-#endif
-struct field_name {
+
+/* field_name 为字段 */
+struct field_name
+{
     char* key;
     char* value;
     struct field_name* next;
 };
-struct httpMessage {
+/* httpMessage 为 http 报文 */
+struct httpMessage
+{
     char* method;
     char* url;
     char* version;
     struct field_name* pField;
 };
+
 void getUserInfo(char* buf);
 void httpMessageInitalize(struct httpMessage* pMessage,int size,const char* method,const char* url,const char* version,const char* host);
 void httpMessageFree(struct httpMessage* pMessage);
 void httpMessageAddField(struct httpMessage* pMessage,const char*key,const char*value);
 void createHttpMessage(char *buf,struct httpMessage* pMessage);
+
 #endif
